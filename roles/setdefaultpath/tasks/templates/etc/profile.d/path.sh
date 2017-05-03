93ba5ea28d8de73bf0bda1eb569ec452c125e257
pgonyan <pgonyan@gmail.com>
2017-05-03 12:15:00 +0100

    path.sh added

--- roles/setdefaultpath/tasks/templates/etc/profile.d/path.sh
+++
@@ -1,4 +0,0 @@
-export PATH=/opt/jdk1.8.0_121/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-
-export JAVA_HOME=/opt/jdk1.8.0_121
-export CLASSPATH="/opt/jdk1.8.0_121/lib:."
--- roles/setdefaultpath/template/etc/profile.d/path.sh
+++ roles/setdefaultpath/template/etc/profile.d/path.sh
@@ -1,5 +1,4 @@
-export PATH=/opt/jdk1.8.0_121/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export PATH=/opt/jdk1.8.0_121/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-
-export JAVA_HOME=/opt/jdk1.8.0_20
-export CLASSPATH="/opt/jdk1.8.0_20/lib:."
+export JAVA_HOME=/opt/jdk1.8.0_121
+export CLASSPATH="/opt/jdk1.8.0_121/lib:."
