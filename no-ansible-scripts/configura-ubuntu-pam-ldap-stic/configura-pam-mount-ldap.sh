32c206d109685edfd541dcbb8375dbafec9c5dd1
root <pgonyan@gmail.com>
2018-11-20 09:55:40 +0000

    ok

--- no-ansible-scripts/configura-ubuntu-pam-ldap-stic/configura-pam-mount-ldap.sh
+++ no-ansible-scripts/configura-ubuntu-pam-ldap-stic/configura-pam-mount-ldap.sh
@@ -1,4 +1,14 @@
+
 #!/bin/bash --login
+
+
+apt-get install -y libpam-ldap auth-client-config ldap-auth-client ldap-auth-config libnss-ldap ldap-utils nscd libpam-script
+cifs-utils nfs-common
+chown root.root /etc/sssd/sssd.conf
+chmod go-r  /etc/sssd/sssd.conf
+
+service sssd restart
+
 
 
 cd /soft/configura-ubuntu-pam-ldap-stic
