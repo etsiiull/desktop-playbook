c089d990c4fa3ab45833010496e4ac54faec54aa
Pedro Gonzalez <pgonyan@ull.es>
2019-02-21 12:42:55 +0000

    path

--- roles/profile-env-alias-scripts/template/etc/profile.d/path.sh
+++ roles/profile-env-alias-scripts/template/etc/profile.d/path.sh
@@ -1,4 +1,4 @@
 export PATH=/opt/node-v6.11.5-linux-x64/bin:/opt/jdk1.8.0_121/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-export JAVA_HOME=/opt/jdk1.8.0_121
-export CLASSPATH="/opt/jdk1.8.0_121/lib:."
+export JAVA_HOME=/opt/jdk1.8.0_20
+export CLASSPATH="/opt/jdk1.8.0_20/lib:."
